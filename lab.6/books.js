31b2db1f25a9e69cc5a0676766e8f7575d87fed4
Razmahnina Margarita <ritysiav@gmail.com>
2025-09-04 19:40:34 +0700

    Книги

---
+++ lab.6/books.js
@@ -0,0 +1,82 @@
+const books = [
+    {
+        author: 'Лао-цзы',
+        name: 'Дао дэ цзин. Канон о Дао и дэ',
+        publicationYear: 2022,
+        price: '450 ₽',
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Карл Саган',
+        name: 'Космос',
+        publicationYear: 2024,
+        price: '750 ₽',
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Баладжи Касал',
+        name: 'Ошибки разумного инвестора',
+        publicationYear: 2025,
+        price: '990 ₽',
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Джордж Мартин',
+        name: 'Игра престолов',
+        publicationYear: 1996,
+        price: '760 ₽',
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Джоан Роулинг',
+        name: 'Гарри Поттер и филосовский камень',
+        publicationYear: 1997,
+        price: '880 ₽',
+        bestseller: true,
+        available: true
+    },
+    {
+        author: 'Джон Стейнбек',
+        name: 'О мышах и людях',
+        publicationYear: 1937,
+        price: '490 ₽',
+        bestseller: false,
+        available: false
+    },
+    {
+        author: 'Рэй Брэдбери',
+        name: '451° по Фаренгейту',
+        publicationYear: 1953,
+        price: '600 ₽',
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Стивен Хокинг',
+        name: 'Теория всего',
+        publicationYear: 2002,
+        price: '680 ₽',
+        bestseller: false,
+        available: true
+    },
+    {
+        author: 'Франц Кафка',
+        name: 'Процесс',
+        publicationYear: 1925,
+        price: '800 ₽',
+        bestseller: true,
+        available: false
+    },
+    {
+        author: 'Дэниел Киз',
+        name: 'Цветы для Элджернона',
+        publicationYear: 1966,
+        price: '610 ₽',
+        bestseller: false,
+        available: true
+    }
+];
