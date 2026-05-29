85d47cf27de0057fa2ba3ca33d47b5c9192189e5
Маргарита Размахнина <ritysiav@gmail.com>
2026-05-30 04:05:58 +0700

    Доп

---
+++ lab6/F1.js
@@ -0,0 +1,83 @@
+// Массив с информацией о пилотах Формулы 1
+const pilots = [
+    {
+        name: "Макс Ферстаппен",
+        team: "Red Bull Racing",
+        country: "Нидерланды",
+        age: 28,
+        championships: 4,
+        photo: "https://i.pinimg.com/736x/f2/45/5d/f2455d801cf28853b068d39259d1bc33.jpg"
+    },
+    {
+        name: "Льюис Хэмилтон",
+        team: "Ferrari",
+        country: "Великобритания",
+        age: 41,
+        championships: 7,
+        photo: "https://i.pinimg.com/736x/0a/29/30/0a2930e731482a309ce06f2d490098b4.jpg"
+    },
+    {
+        name: "Шарль Леклер",
+        team: "Ferrari",
+        country: "Монако",
+        age: 28,
+        championships: 0,
+        photo: "https://i.pinimg.com/736x/b7/15/ae/b715ae8a5e105484c980b439cebd7598.jpg"
+    },
+    {
+        name: "Ландо Норрис",
+        team: "McLaren",
+        country: "Великобритания",
+        age: 26,
+        championships: 1,
+        photo: "https://i.pinimg.com/1200x/7d/a5/fd/7da5fd8d60bf74a9693e9073ddc99e2f.jpg"
+    },
+    {
+        name: "Фернандо Алонсо",
+        team: "Aston Martin",
+        country: "Испания",
+        age: 44,
+        championships: 2,
+        photo: "https://i.pinimg.com/1200x/2f/f2/18/2ff218d15d1169cb50c3db26064c2d5f.jpg"
+    },
+    {
+        name: "Карлос Сайнс",
+        team: "Williams",
+        country: "Испания",
+        age: 31,
+        championships: 0,
+        photo: "https://i.pinimg.com/736x/54/14/82/54148203b60bce9c287fa52adf5305c5.jpg"
+    },
+    {
+        name: "Оскар Пиастри",
+        team: "McLaren",
+        country: "Австралия",
+        age: 25,
+        championships: 0,
+        photo: "https://i.pinimg.com/736x/9a/3e/36/9a3e3637ba9df9668efdf110f6d7b7f1.jpg"
+    },
+    {
+        name: "Серхио Перес",
+        team: "Cadillac",
+        country: "Мексика",
+        age: 36,
+        championships: 0,
+        photo: "https://i.pinimg.com/736x/84/89/8f/84898faee4126b2c5b7af9f536b0f0e1.jpg"
+    },
+    {
+        name: "Джордж Расселл",
+        team: "Mercedes",
+        country: "Великобритания",
+        age: 28,
+        championships: 0,
+        photo: "https://i.pinimg.com/736x/31/63/e3/3163e37644d480927badaa127677beac.jpg"
+    },
+    {
+        name: "Кими Антонелли",
+        team: "Mercedes",
+        country: "Италия",
+        age: 19,
+        championships: 0,
+        photo: "https://i.pinimg.com/736x/4f/40/12/4f4012d7ffec1216e21b9b01150eee00.jpg"
+    }
+];
